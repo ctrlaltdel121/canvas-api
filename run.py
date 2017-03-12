a1bd5a1cf5e8e4fca42ce29192a137176d4a92bd
Jeremy Williams <ctrl.alt.del.121@gmail.com>
2017-03-12 17:52:00 -0400

    rename index to bust cache

--- run.py
+++ run.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return app.send_static_file('index.html')
+    return app.send_static_file('index2.html')
 
 @app.route('/user_input', methods=['POST'])
 def processInput():
